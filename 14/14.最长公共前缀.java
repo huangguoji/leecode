1bd88126d2d21e4c8e016f1bef676932a7a8bfc8
huanggj2 <huangguoji@126.com>
2021-09-08 01:00:50 +0800

    14

---
+++ 14/14.最长公共前缀.java
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=java
+ *
+ * [14] 最长公共前缀
+ *
+ * https://leetcode-cn.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (40.77%)
+ * Likes:    1767
+ * Dislikes: 0
+ * Total Accepted:    608K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：strs = ["flower","flow","flight"]
+ * 输出："fl"
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：strs = ["dog","racecar","car"]
+ * 输出：""
+ * 解释：输入不存在公共前缀。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] 仅由小写英文字母组成
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int end = 0;
+        int minlength = strs[0].length();
+        for(String item:strs){
+            minlength = Math.min(minlength, item.length());
+        }
+        for(int i=0;i<minlength;i++){
+            for (int k=0;k<strs.length-1;k++) {
+                if(strs[k].charAt(i)!=strs[k+1].charAt(i)){
+                     break;
+                }
+                else{
+                    if(k+1==strs.length-1)
+                        end=i;
+                }
+            }
+        }
+        return strs[0].substring(0,end);
+    }
+}
+// @lc code=end
+
